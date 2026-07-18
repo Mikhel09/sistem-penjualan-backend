0d248f24b4d2d90cd93e5670bfacae317acb7b7b
Mikhel09 <66584105+Mikhel09@users.noreply.github.com>
2026-07-19 00:08:14 +0700

    update cors dengan url vercel asli

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-     origin: ['http://localhost:5173'],
+     origin: ['http://localhost:5173', 'https://sistem-penjualan-frontend.vercel.app'],
    }));
 app.use(express.json());
 
